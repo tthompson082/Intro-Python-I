545669c955b0fe026de6cecf0f98c5e706fb1029
Tyler Thompson <tthompson082@gmail.com>
2020-02-10 13:40:11 -0500

    Add solution for 05

--- src/05_lists.py
+++ src/05_lists.py
@@ -8,22 +8,31 @@ y = [8, 9, 10]
 
 # Change x so that it is [1, 2, 3, 4]
 # YOUR CODE HERE
+x.append(4)
 print(x)
 
 # Using y, change x so that it is [1, 2, 3, 4, 8, 9, 10]
 # YOUR CODE HERE
+x.extend(y)
 print(x)
 
 # Change x so that it is [1, 2, 3, 4, 9, 10]
 # YOUR CODE HERE
+x.remove(8)
 print(x)
 
 # Change x so that it is [1, 2, 3, 4, 9, 99, 10]
 # YOUR CODE HERE
+x.insert(5, 99)
 print(x)
 
 # Print the length of list x
 # YOUR CODE HERE
+z = len(x)
+print(z)
 
 # Print all the values in x multiplied by 1000
 # YOUR CODE HERE
+a = sum(x)
+b = a * 1000
+print(b)
